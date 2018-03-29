2929591c4b05b77d9385c64326da64c345c827f3
sponditap <sponditap@gmail.com>
2018-03-30 01:10:32 +0530

    Update regn.php

--- regn.php
+++ regn.php
@@ -6,14 +6,15 @@ $lname=mysqli_real_escape_string($con, $_POST['lastname']);
 $username=mysqli_real_escape_string($con, $_POST['username']);
 $password=mysqli_real_escape_string($con, $_POST['pass']);
 $dob=mysqli_real_escape_string($con, $_POST['dob']);
+$gender=mysqli_real_escape_string($con, $_POST['gender']);
 $emailid=mysqli_real_escape_string($con, $_POST['emailid']);
 $phoneno=mysqli_real_escape_string($con, $_POST['phoneno']);
 
-$sql="INSERT INTO register(frstname,lastname,username,pass,dob,emailid,phoneno) VALUES('$fname','$lname','$username','$password','$dob','$emailid','$phoneno')";
+$sql="INSERT INTO registration(frstname,lastname,username,pass,gender,dob,emailid,phoneno) VALUES('$fname','$lname','$username','$password', '$gender', '$dob','$emailid','$phoneno')";
 
 if (($con->query($sql) === TRUE)) {
            echo "Customer created successfully \n";
-           header('location:yes.php');
+           header('location:login.php');
 		}
 else
 	echo "Error: " . $sql . "<br>" . $con->error;
