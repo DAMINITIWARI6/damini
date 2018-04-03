62539657c183cde2266da2d2fc181874adbc09ef
DAMINITIWARI6 <daminitiwari6@gmail.com>
2018-04-04 02:25:49 +0530

    Add files via upload

--- adminvalid.php
+++ adminvalid.php
@@ -1,5 +1,6 @@
 <?php
 	
+	include"navbar.php";
 	
 	$con = mysqli_connect("localhost","root","");
 	
@@ -37,6 +38,65 @@
 			unset($_SESSION['SName']);
 		    }
 		$_SESSION['Status']="Session Expired!!!";
-		echo "<h2>Login Failed!!!</h2>";
+		
 	}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	<title>Registration</title>
+	<meta charset="utf-8">
+	<style>
+		body
+		{
+			width: 100%;
+		height: 100%;
+
+    background-image: url("image/reg.png") ;
+	background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;overflow-x: 
+		}
+
+				div.error
+		{
+			width: 300px;
+		height:300px;
+		background-color: rgba(0,0,0,0.65);
+		margin: 0 auto;
+		margin-top: 15px;
+		padding-top: 10px;
+		padding-left: 30px;
+		padding-right: 20px;
+		border-radius: 15px;
+		color: white;
+		font-weight: bolder;
+		box-shadow: 4px 4px rgba(1,1,1,1);
+		background-image: url("image/error.png");
+		background-size: fill;
+		 background-repeat: no-repeat;
+		
+		}
+		h3{
+			color: black;
+			font-style: bolder;
+
+
+		}
+		</style>
+	</head>
+	<body>
+		<div class="error">
+			
+	<form  name ="myForm" action='regn.php' method='POST' autocomplete="on">
+		
+		</form>
+
+			
+		</div>
+<center>
+<h3 style= "text-align:bottom"> <a href="admin_login.php "> LogIn Failed!!<br>Please LogIn again!!</a></h3></center>
+	</body>
+	</html>
